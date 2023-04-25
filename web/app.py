306057d5dafcdd94245b87593a895ee11a92bd3e
Haseebk0678 <haseebkhan0678@gmail.com>
2023-04-25 10:32:22 -0400

    feat: worked on creating different types of models with different parameters

--- web/app.py
+++ web/app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+input_shape = (100,100)
 
 # define a route for the image upload
 @app.route('/predict', methods=['POST'])
@@ -21,10 +22,16 @@ def predict():
     image = request.files['image'].read()
     image = np.fromstring(image, np.uint8)
     image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
-    image = cv2.resize(image, (100, 100))
-    image = np.expand_dims(image, axis=-1)
-    image = np.expand_dims(image, axis=0)
+    image = cv2.resize(image, input_shape)
+
+    # Convert the image to a numpy array
+    image_array = np.array(image)
     
+    # Resize the image to the correct dimensions (if necessary)
+    image_array = np.resize(image_array, (1, 100, 100))
+    
+    # Normalize the image data
+    image_array = image_array / 255.0
     # use the CNN model to predict if a mask is present
     prediction = model.predict(image)[0][0]
     if prediction > 0.5:
